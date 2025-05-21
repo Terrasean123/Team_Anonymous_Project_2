1dbe998e1126e768ec7bca1ae2a5e2ccc8bf0ed0
Sean Makoka <makokatinse@gmail.com>
2025-05-21 18:36:06 +1000

    added settings file

---
+++ TeamAnonymous_Project-main/settings.php
@@ -0,0 +1,6 @@
+<?php
+    $host = "localhost";         // because XAMPP runs the server locally
+    $user = "root";          // default username for XAMPP's MySQL
+    $pwd = "";              // default password is empty in XAMPP
+    $sql_db = "eoi_db";  // replace with the actual name of your database
+?>
