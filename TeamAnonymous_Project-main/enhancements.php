53fa93231eb1dd78518bc83d6a89abfbc65b7816
mustafaj1122 <106033426@student.swin.edu.au>
2025-05-26 21:48:09 +1000

    created db_connection_users.php

---
+++ TeamAnonymous_Project-main/enhancements.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once("settings.php");
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Get user input
+$username = trim($_POST['username']);
+$password = trim($_POST['password']);
+
+// Simple query to check credentials
+$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($conn, $query);
+$user = mysqli_fetch_assoc($result);
+
+if ($user) {
+  $_SESSION['username'] = $user['username'];
+  header("Location: welcome.php");
+  exit();
+} else {
+  echo "❌ Incorrect username or password.";
+}
+?>
