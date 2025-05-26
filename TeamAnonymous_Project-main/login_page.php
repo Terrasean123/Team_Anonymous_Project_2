ce5f49721036b0282f019fdcc3836e9cf273f3e1
mustafaj1122 <106033426@student.swin.edu.au>
2025-05-26 21:57:22 +1000

    added username and login forms

--- TeamAnonymous_Project-main/login_page.php
+++ TeamAnonymous_Project-main/login_page.php
@@ -1,2 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Manage EOI</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <?php include 'header.inc'; ?>
+    <div id="manage-content">
+        <h1>Manager Login</h1>
+        <form class="username_text" method="GET" action="enhancements.php">
+            <input type="text" placeholder = "Enter username" name="username">
+        </form>
+        <form class="password_text" method="GET" action="enhancements.php">
+            <input type="password" placeholder = "Enter password" name="password">
+        </form>
+    <div>
+</body>
+</html>
 <?php
-    
+
+?>
